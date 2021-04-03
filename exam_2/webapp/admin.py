e5106ada6ed0bc8e81f605e107be3a1c82f42f5d
German <coolmatoff@gmail.com>
2021-04-03 12:19:54 +0600

    created super user, models

--- exam_2/webapp/admin.py
+++ exam_2/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from webapp.models import Poll, Choice
 
 # Register your models here.
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'created_at', 'question']
+    fields = ['question']
+    readonly_fields = ['id', 'created_at']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'choice_text']
+    fields = ['choice_text', 'poll']
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- exam_2/webapp/models.py
+++ exam_2/webapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Poll(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
+    question = models.CharField(null=False, blank=False, max_length=300)
+
+    class Meta:
+        db_table = 'polls'
+        verbose_name = 'Poll'
+        verbose_name_plural = 'Polls'
+
+    def __str__(self):
+        return f'{self.question}'
+
+
+class Choice(models.Model):
+    choice_text = models.CharField(null=False, blank=False, max_length=300)
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, verbose_name='Poll', related_name='polls', default=1)
+
+    class Meta:
+        db_table = 'choices'
+        verbose_name = 'Choice'
+        verbose_name_plural = 'Choices'
+
+    def __str__(self):
+        return f'{self.choice_text}'
